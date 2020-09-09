088860fd096970d67fa537c2f5f697bcb8ca29cb
Santhosh-23mj <33282970+Santhosh-23mj@users.noreply.github.com>
2020-09-09 12:33:13 +0000

    Update pastebinscrape.py

--- pastebinscrape.py
+++ pastebinscrape.py
@@ -1,101 +1,97 @@
 #!/usr/bin/python3
 
 import os
+import time
 import argparse
-import threading
-import mechanize
+import urllib.request
+import concurrent.futures
 from bs4 import BeautifulSoup
 
 linkDic = {}
 
-def getPage(url):
-	"""Function to get the HTML document from the specified URL"""
-	browser = mechanize.Browser()
-	browser.set_handle_robots(False)
-	webPage = browser.open(url)
-	return webPage
-
-
-def getNoofPages(webPage):
-	"""Function to get the number of pages the users has pastes"""
-	soup = BeautifulSoup(webPage, 'html.parser')
-	pages = len(soup.find("div", attrs = {'class' : 'pagination'}).findAll('a'))
-	return pages
+def getHTML(url):
+	webPage = urllib.request.urlopen(url)
+	return webPage.read()
 
 
 def getLinks(webPage):
-	"""Function to parse the webpage and fetch links"""
 	soup = BeautifulSoup(webPage, 'html.parser')
-	
-	# Fetch all table data <td> tags
 	tableData = soup.find_all('td')
-	
-	# Get all <a> tags from <td> with links
 	for td in tableData:
 		link = td.find('a')
 		if(link and not link.text == 'None'):
-			uri = "https://www.pastebin.com"+str(link.get('href'))
+			uri = "https://pastebin.com" + str(link.get('href'))
 			linkDic[link.text] = uri
 
+ 
+def getPageNo(url):
+	webPage = getHTML(url)
+	soup    = BeautifulSoup(webPage, 'html.parser')
+	pageNo  = len(soup.find('div',attrs = {'class':'pagination'}).findAll('a'))
+	return webPage, pageNo
 
-def printLinks():
-	"""Function to print the values in the Dictionary"""
-	with open("links.txt",'w') as f:
+
+def writeLinks():
+	print("---------------------------------------------------------")
+	with open('links.txt',"w") as f:
 		for name, link in linkDic.items():
-			print("{:<25}  {:<25}".format(name,link))
-			f.write("{:<25}  {}\n".format(name,link))
+			print("{:<25} {}".format(name, link))
+			f.write("{:<25} {}\n".format(name, link))
+	print("---------------------------------------------------------")
 
 
 def createFile(filename, data):
-	"""Function to create a file for each data from each link"""
 	with open(filename,"w") as f:
 		f.write(data)
 
 
-def getData(username):
-	"""Function to fetch the data from each links"""
-	for name, link in linkDic.items():
-		webPage  = getPage(link)
-		soup     = BeautifulSoup(webPage, 'html.parser')
-		data     = soup.find('textarea').text
-		filename = os.path.join(username,name)
-		print("[+] Getting data from {} and writing to {} !".format(link, filename))
-		createFile(filename, data)
+def getData(nameLinkTuple):
+	webPage = getHTML(nameLinkTuple[1])
+	soup    = BeautifulSoup(webPage, 'html.parser')
+	data    = soup.find('textarea').text
+	# Comment this if you donot want to fill the screen.
+	print("[+] Getting data from {} - {}".format(nameLinkTuple[0], nameLinkTuple[1]))
+	createFile(nameLinkTuple[0], data)
 
 
 def main():
-	"""Main function"""
-	
+	print("[!] Starting at : " + str(time.asctime()))
+	print("=========================================================")
+	startTime = time.time()
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-u","--username", help = "specify the pastebin username", required = True)
-	parser.add_argument("-d","--data", help = "To get the data from all the links", action = 'store_true')
+	parser.add_argument("-u","--username", help = "Specify the username of the pastebin account", required = True)
+	parser.add_argument("-d","--data", help = "If you want to download the pastes", action = 'store_true')
+	parser.add_argument("-t","--threads", help = "No. of threads to run", type = int, default = 10)
 	args = parser.parse_args()
 	
 	url = "https://pastebin.com/u/" + args.username + "/1"
 	
-	# Fetching the HTML document
-	webPage = getPage(url)
+	webPage, pageNo = getPageNo(url)
 	
-	# Parsing the document to find the number of pages
-	pageNo = getNoofPages(webPage)
+	print("[*] The user has {} pages".format(pageNo-1))
+	print("[+] Getting Links from Page : 1")
+	getLinks(webPage)
 	
-	# Parsing the pages to get the links
-	for i in range(1,pageNo):
-		url = "https://pastebin.com/u/" + args.username + "/" + str(i)
-		webPage = getPage(url)
-		getLinks(webPage)
-	printLinks()
-	
+	if(pageNo > 2):
+		for i in range(2,pageNo):
+			print("[+] Getting Links from Page : " + str(i))
+			url     = "https://pastebin.com/u/" + args.username + "/" + str(i)
+			webPage = getHTML(url)
+			getLinks(webPage)
+	writeLinks()
+	print("[+] Wrote all links to links.txt")
 	
 	if(args.data):
 		if(not os.path.isdir(args.username)):
 			os.mkdir(args.username)
-		print()
-		print("[+] Getting data from all pastes !")
-		getData(args.username)
-		print("[+] Done !")
-		print()
+		os.chdir(args.username)
+		print("[+] Getting data from all Pastes !")
+		with concurrent.futures.ThreadPoolExecutor(max_workers = args.threads) as Executor:
+			Executor.map(getData, list(linkDic.items()))
+	print("[+] Done !")
+	print("=========================================================")
+	print("[!] Finished at      : " + str(time.asctime()))
+	print("[+] Time to complete : {}s".format(str(time.time() - startTime)[:-12]))
 	
-
 if(__name__ == "__main__"):
 	main()
